9fa9bb315d0166b9ceb3054c5f45b125dad511ef
yasmine-toumi <yasmine.ettoumi10@gmail.com>
2021-02-25 11:56:40 +0100

    all

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group([
+
+    'middleware' => 'api'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
